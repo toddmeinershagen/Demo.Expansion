ec6649d084b9f487286a11a02ba46974c0d90888
Todd Meinershagen <todd@meinershagen.net>
2012-02-07 14:48:14 -0600

    removed the original form

--- Demo.Expansion/Program.cs
+++ Demo.Expansion/Program.cs
@@ -15,7 +15,7 @@ namespace Demo.Expansion
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new K1OptionsForm());
 
         }
     }
