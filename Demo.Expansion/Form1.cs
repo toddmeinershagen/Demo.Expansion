b279a5fa2199f183c355662efb9262c5f3050082
Todd Meinershagen <todd@meinershagen.net>
2012-02-07 14:30:03 -0600

    initial commit

---
+++ Demo.Expansion/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Demo.Expansion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Options = new List<string>();
+            //Options = new List<string> { "Option1" };
+            //Options = new List<string> {"Option1", "Option2", "Option3", "Option4", "Option5"};
+            //Options = GetOptions();
+            //BindOptions();
+        }
+
+        private void BindOptions()
+        {
+
+            OptionsLayoutPanel.RowCount = Options.Count;
+            
+            for (int i = 0; i < Options.Count; i++)
+            {
+                var button = new RadioButton();
+                button.Text = Options[i];
+
+                var position = new TableLayoutPanelCellPosition(0, i);
+                OptionsLayoutPanel.SetCellPosition(button, position);
+                OptionsLayoutPanel.Controls.Add(button);
+            }
+        }
+
+        private List<string> GetOptions()
+        {
+            const int maxOptions = 20;
+            var numberGenerator = new Random();
+            var numberOfOptions = numberGenerator.Next(maxOptions);
+
+            var options = new List<string>();
+            for (int i = 0; i < numberOfOptions; i++)
+            {
+                options.Add(string.Format("Option{0}", i));
+            }
+
+            return options;
+        }
+
+        public List<string> Options { get; set; }
+
+        private void GenerateButton_Click(object sender, EventArgs e)
+        {
+            Options = GetOptions();
+            BindOptions();
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void HorizontalLine_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Demo.Expansion/K1OptionsForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Demo.Expansion
+{
+    public partial class K1OptionsForm : Form
+    {
+        public K1OptionsForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
